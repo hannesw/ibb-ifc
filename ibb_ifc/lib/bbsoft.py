ab340528eda1cf902d711c5fcf16ed76cd1c6363
Hannes Wörn <hannes@woern.de>
2024-02-04 12:02:30 +0100

    Refactor manhole name correction and property type change logic

--- ibb_ifc/lib/bbsoft.py
+++ ibb_ifc/lib/bbsoft.py
@@ -65,28 +65,27 @@ def _to_float(value):
 
 
 def _correct_manhole_names(model):
-    # Look for the properties "ID_DRAIN1" and "ID_DRAIN2" in the Schacht pset. Remove ",-" from the end of the strings.
-    for sewer in ifcopenshell.util.selector.filter_elements(model, "IfcFlowSegment, IfcFlowTreatmentDevice"):
-        psets = ifcopenshell.util.element.get_psets(sewer)
-        for pset in psets:
-            print(pset.Name)
-        if "Haltung" not in psets and "Leitung" not in psets:
+    sewers = ifcopenshell.util.selector.filter_elements(
+        model, "IfcFlowSegment,Haltung.ID_NAME != NULL")
+    for s in sewers:
+        pset = ifcopenshell.util.element.get_pset(s, "Haltung")
+        # Skip if keyerror
+        try:
+            pset["ID_DRAIN1"] = pset["ID_DRAIN1"].replace(",-", "")
+            pset["ID_DRAIN2"] = pset["ID_DRAIN2"].replace(",-", "")
+        except KeyError:
             continue
-        if "Haltung" in psets:
-            # skip if key error ID_DRAIN1 or ID_DRAIN2. It's likely that this is not a valid sewer with geometry
-            if "ID_DRAIN1" not in psets["Haltung"] or "ID_DRAIN2" not in psets["Haltung"]:
-                continue
-            psets["Haltung"]["ID_DRAIN1"] = psets["Haltung"]["ID_DRAIN1"].replace(
-                ",-", "")
-            psets["Haltung"]["ID_DRAIN2"] = psets["Haltung"]["ID_DRAIN2"].replace(
-                ",-", "")
-        else:
-            if "ID_DRAIN1" not in psets["Leitung"] or "ID_DRAIN2" not in psets["Leitung"]:
-                continue
-            psets["Leitung"]["ID_DRAIN1"] = psets["Leitung"]["ID_DRAIN1"].replace(
-                ",-", "")
-            psets["Leitung"]["ID_DRAIN2"] = psets["Leitung"]["ID_DRAIN2"].replace(
-                ",-", "")
+
+    branches = ifcopenshell.util.selector.filter_elements(
+        model, "IfcFlowTreatmentDevice")
+    for b in branches:
+        pset = ifcopenshell.util.element.get_pset(b, "Leitung")
+        # Skip if keyerror
+        try:
+            pset["ID_DRAIN1"] = pset["ID_DRAIN1"].replace(",-", "")
+            pset["ID_DRAIN2"] = pset["ID_DRAIN2"].replace(",-", "")
+        except KeyError:
+            continue
 
 
 def _change_property_type(model, name, dimension_factor):
@@ -111,22 +110,26 @@ def _change_property_type(model, name, dimension_factor):
                 "IfcPositiveLengthMeasure", value)
             prop.NominalValue = length_measure
 
-    print(f"Changed {total} properties {name}")
+    print(f"\tChanged {total} properties {name}")
 
 
 def process(ifc_file_path) -> str:
     if not ifc_file_path.endswith(".ifc"):
         raise ValueError("Invalid file format. Only IFC files are supported.")
 
+    print(f"Opening {ifc_file_path}")
     new_file_path = os.path.splitext(ifc_file_path)[0] + "_processed.ifc"
+    print(f"New file path will be {new_file_path}")
     model = ifcopenshell.open(ifc_file_path)
 
+    print(f"Correcting manhole names")
     _correct_manhole_names(model)
 
+    print(f"Changing property types:")
     for name, dimension_factor in replacing_properties:
         _change_property_type(model, name, dimension_factor)
 
-    print(f"Saving modified file to {new_file_path}")
+    print(f"Saving modified file")
     model.write(new_file_path)
     print(f"Modified IFC file saved as {new_file_path}")
 
