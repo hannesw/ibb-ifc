b78279028de753cd0bb2c25725e573958299b6e1
Hannes Wörn <hannes@woern.de>
2024-03-18 12:36:00 +0100

    Code cleanup

--- ibb_ifc/lib/model_checker.py
+++ ibb_ifc/lib/model_checker.py
@@ -2,20 +2,23 @@ import ifcopenshell
 import ezdxf
 from ezdxf import zoom
 
-"""
-Extracts the placement matrix from an IFC element and returns the coordinates.
-"""
 
-
-def _coords(element):
-    matrix = ifcopenshell.util.placement.get_local_placement(
-        element.ObjectPlacement)
-    return matrix[:, 3][:3]
-
-
-"""
-Calculates the midpoint of an sewer or lateral and returns the coordinates.
-"""
+def _draw_check_marker(msp, coords, text=None, color=1, layer='0'):
+    msp.add_circle(
+        center=(coords[0], coords[1]),
+        radius=3,
+        dxfattribs={
+            'color': color,
+            'layer': layer
+        })
+    if text:
+        msp.add_mtext(text, dxfattribs={
+            'color': color,
+            'char_height': 0.6,
+            'attachment_point': 5,  # align text mid-center
+            'layer': layer
+        },
+        ).set_location((coords[0], coords[1]))
 
 
 def _sewer_midpoint(pset):
@@ -29,6 +32,16 @@ def _sewer_midpoint(pset):
     return (x1 + x2) / 2, (y1 + y2) / 2
 
 
+def _sewer_check_slope(pset):
+    try:
+        slope = pset["HYD_GRAD_PP"]
+    except KeyError:
+        return None
+    if slope < 0.01:
+        return slope
+    return None
+
+
 def run(ifc_file_path: str) -> None:
     model = ifcopenshell.open(ifc_file_path)
 
@@ -40,39 +53,17 @@ def run(ifc_file_path: str) -> None:
         model, "IfcFlowSegment,Haltung.ID_NAME != NULL")
     for s in sewers:
         pset = ifcopenshell.util.element.get_pset(s, "Haltung")
-        # Skip if keyerror
-        try:
-            slope = pset["HYD_GRAD_PP"]
-        except KeyError:
-            continue
-        if slope < 0.01:
-            print(
-                f"Slope of {pset['ID_NAME']} is too low: {round(slope * 1000, 1)} ‰")
-            # coords = _coords(s) # This is a more general approach but makes it difficult to identify the sewer
-            coords = _sewer_midpoint(pset)
-            msp.add_circle(center=(coords[0], coords[1]),
-                           radius=3, dxfattribs={'color': 1})
-            msp.add_mtext(f"Gefälle: {round(slope * 1000, 1)} ‰",
-                          dxfattribs={'color': 1, 'char_height': 0.6, 'attachment_point': 5}).set_location((coords[0], coords[1]))
+        if _sewer_check_slope(pset):
+            _draw_check_marker(
+                msp, _sewer_midpoint(pset), f"Gefälle: {round(pset["HYD_GRAD_PP"] * 1000, 1)} ‰")
 
         laterals = ifcopenshell.util.selector.filter_elements(
             model, "IfcFlowTreatmentDevice,Leitung.ID_NAME != NULL")
         for l in laterals:
             pset = ifcopenshell.util.element.get_pset(l, "Leitung")
-            # Skip if keyerror
-            try:
-                slope = pset["HYD_GRAD_PP"]
-            except KeyError:
-                continue
-            if slope < 0.01:
-                print(
-                    f"Slope of {pset['ID_NAME']} is too low: {round(slope * 1000, 1)} ‰")
-                # coords = _coords(l) # This is a more general approach but makes it difficult to identify the lateral
-                coords = _sewer_midpoint(pset)
-                msp.add_circle(center=(coords[0], coords[1]),
-                               radius=3, dxfattribs={'color': 5})
-                msp.add_mtext(f"Gefälle: {round(slope * 1000, 1)} ‰",
-                              dxfattribs={'color': 5, 'char_height': 0.6, 'attachment_point': 5}).set_location((coords[0], coords[1]))
+            if _sewer_check_slope(pset):
+                _draw_check_marker(
+                    msp, _sewer_midpoint(pset), f"Gefälle: {round(pset["HYD_GRAD_PP"] * 1000, 1)} ‰")
 
     zoom.extents(msp)
     dxf_file.saveas(f'{ifc_file_path}.dxf'.replace('.ifc', ''))
