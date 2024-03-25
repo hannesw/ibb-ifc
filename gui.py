84b2af6318ce65630fda8c06751f42d5a2a526f2
Hannes Wörn <hannes@woern.de>
2024-03-25 21:07:32 +0100

    Add file selection functionality and processing logic***

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 import customtkinter
 import tkinter.filedialog as filedialog
+import lib.bbsoft as bbsoft
+import lib.model_checker as model_checker
+import lib.quantity_takeoff as qto
 
 
 class App(customtkinter.CTk):
@@ -11,19 +14,27 @@ class App(customtkinter.CTk):
 
         self.geometry("400x150")
 
-        self.button = customtkinter.CTkButton(
-            self, text="Datei auswählen", command=self._select_file
+        self.file_button = customtkinter.CTkButton(
+            self, text="IFC-Datei auswählen", command=self._select_file
         )
-        self.button.pack(padx=20, pady=20)
+        self.file_button.pack(padx=20, pady=20)
 
         self.file_label = customtkinter.CTkLabel(master=self, text="")
         self.file_label.pack()
+
+        self.run_button = customtkinter.CTkButton(
+            self, text="Datei verarbeiten", state="disabled", command=self._run
+        )
+        self.run_button.pack(padx=20, pady=20)
+
+        self.progress_bar = customtkinter.CTkProgressBar(self, mode="indeterminate")
 
     def _select_file(self):
         self.filename = filedialog.askopenfilename(
             title="Bitte eine Datei auswählen", filetypes=[("IFC", "*.ifc")]
         )
         self._update_filename_label()
+        self.run_button.configure(state="normal")
 
     def _update_filename_label(self):
         if self.filename:
@@ -35,6 +46,27 @@ class App(customtkinter.CTk):
             if hasattr(self, "file_label"):  # Remove label if filename is cleared
                 self.file_label.destroy()
                 del self.file_label
+
+    def _update_progress_bar(self, operation: str):
+        # check if progress bar is already packed
+        if not self.progress_bar.winfo_ismapped():
+            self.progress_bar.pack()
+        if operation == "start":
+            self.progress_bar.start()
+        elif operation == "stop":
+            self.progress_bar.stop()
+
+    def _run(self):
+        if not self.filename:
+            self.show_error("Keine Datei ausgewählt")
+            return
+        self._update_progress_bar("start")
+
+        processed_file_path = bbsoft.process(self.filename)
+        model_checker.run(processed_file_path)
+        qto.get(processed_file_path)
+
+        self._update_progress_bar("stop")
 
 
 app = App()
