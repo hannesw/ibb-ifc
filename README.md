ec787a695fad38c47600fb05a56b895b4e24f4d9
Hannes Wörn <hannes@woern.de>
2024-03-27 10:26:01 +0100

    Refactor model_checker.py to improve DXF file generation and add support for converting DXF to DWG

--- README.md
+++ README.md
@@ -1,15 +1,57 @@
 **Hinweis: Das Programm ist in einer sehr frühen Phase und ist als Alpha-Software anzusehen.**
 
-# BBSoft IFC-Verarbeitung
+# Programmbeschreibung
 
-Dieses Programm verarbeitete eine von [BBSoft®](https://bbsoft.de/cont/cont_software.php) erzeugte IFC-Datei. Hierbei werden verschiedene Korrekturen vorgenommen:
+Dieses Programm verarbeitete eine von [BBSoft®](https://bbsoft.de/cont/cont_software.php) erzeugte IFC-Datei, sodass diese mit anderer Software (z.B. für AVA) weiterverarbeitet werden kann.
+Zusätzlich können verschiedene Modellprüfungen für die Qualitätskontrolle durchgeführt werden.
+Ebenfalls kann eine einfache Massenermittlung in Form einer Excel-Datei erzeugt werden.
+
+## BBSoft IFC-Verarbeitung
+
+Folgende werden verschiedene Korrekturen vorgenommen:
 
 - Verwendung von gültigen IfcPropertyTypes. Standardmäßig werden alle Datenbankfelder als String ausgegeben. Diese macht eine Weiterverarbeitung z. B. mit einem AVA-Programm sehr schwierig.
 - Die Textfragmente "(ed.)" und "(ber.)" werden aus den Kanalhaltungen entfernt.
 - An die Schachtnamen wird standardmäßig ein ",-" bzw. falls vorhanden der Straßenname hinzugefügt. Dieses wird entfernt.
 - Verschiedene Angaben werden in die korrekte Einheit (z. B. Meter) umgewandelt.
 
-# Verwendung / Download
+## Modellprüfung
+
+Die gefundenen Problemstellen werden in eine DWG-Datei eingefügt, so dass diese hinter die Planung gelegt werden kann.
+
+### Kanal
+
+- [ ] Vollständigkeit: Material
+- [ ] Vollständigkeit: Dimension
+- [ ] Vollständigkeit: Bauzustand
+- [ ] Vollständigkeit: Wandstärke
+- [ ] Vollständigkeit: Koordinaten, Höhe
+- [ ] Plausibilität: Material
+- [ ] Plausibilität: Dimension (z.B. in Abhängigkeit von Material andere Dimensionen)
+- [ ] Plausibilität: Wandstärke. Bewegt sich die Wandstärke für das angegebene Material + Dimension in den üblichen Grenzen
+- [ ] Plausibilität: Befinden sich die Koordinaten innerhalb von GK oder UTM?
+- [ ] Plausibilität: Ist die Höhe in einem üblichen Rahmen.
+- [ ] Plausibilität: Gibt es Ausreiser bei der Höhe
+- [ ] Plausibilität: Haltungsbezeichnung wie oberer Schacht
+- [ ] Fließrichtung entsprechend Gefälle
+- [ ] Technik: Hydraulische Auslastung
+- [x] Technik: Gefälle ausreichend
+- [ ] Technik: Schacht mit ausreichender Tiefe
+- [ ] Technik: Absturzhöhe
+- [ ] Einlaufhöhe kleiner als Auslaufhöhe
+
+### Wasserleitung
+
+- [ ] Vollständigkeit: Material
+- [ ] Vollständigkeit: Dimension
+- [ ] Vollständigkeit: Bauzustand
+- [ ] Vollständigkeit: Koordinaten, Höhe
+- [ ] Plausibilität: Material
+- [ ] Plausibilität: Befinden sich die Koordinaten innerhalb von GK oder UTM?
+- [ ] Plausibilität: Ist die Höhe in einem üblichen Rahmen.
+- [ ] Plausibilität: Gibt es Ausreiser bei der Höhe
+
+# Verwendung
 
 Aktuelle Binaries für Windows können [hier bei Github](https://github.com/ibb-woern/ibb-ifc/releases) heruntergeladen werden.
 
@@ -17,13 +59,15 @@ Für die graphische Benutzoberfläche starten der `gui.exe`.
 
 Für das Kommandozeilenwerkzeug starten der `cli.exe`. Die verfügbaren Optionen können mit `cli.exe --help` angezeigt werden.
 
+Falls der [ODA File Converter](https://www.opendesign.com/guestfiles/oda_file_converter) installiert ist, wird die Modellprüfung als DWG-Datei ausgegeben. Andernfalls als DXF-Datei.
+
 # Entwicklung
 
 ## Voraussetzungen
 
 - python mit pip
 - Empfehlung für die Verwaltung verschiedener Python-Versionen: [pyenv](https://github.com/pyenv/pyenv) oder [pyenv-win](https://github.com/pyenv-win/pyenv-win)
-- [make](https://www.gnu.org/software/make/) oder [make for windows](https://gnuwin32.sourceforge.net/packages/make.htm)
+- [make](https://cmake.org/)
 
 ## Starten
 
--- lib/model_checker.py
+++ lib/model_checker.py
@@ -1,6 +1,9 @@
+import subprocess
 import ifcopenshell
 import ezdxf
 from ezdxf import zoom
+import os
+from glob import glob
 
 
 def _draw_check_marker(msp, coords, text=None, color=1, layer="0"):
@@ -42,16 +45,79 @@ def _sewer_check_slope(pset):
     return None
 
 
-def run(ifc_file_path: str) -> None:
-    model = ifcopenshell.open(ifc_file_path)
-
+def _setup_dxf() -> ezdxf.layouts.Modelspace:
     dxf_file = ezdxf.new(dxfversion="R2010")
     dxf_file.header["$INSUNITS"] = 6  # in meters
     msp = dxf_file.modelspace()
 
+    dxf_file.layers.new(name="Kanal", dxfattribs={"color": 1})
+    dxf_file.layers.new(name="Wasserleitung", dxfattribs={"color": 5})
+
+    return dxf_file, msp
+
+
+def _convert_to_DWG(dxf_path: str) -> bool:
+    """
+    If the ODA File Converter is installed on the system, this function will
+    convert the file to a DWG file, wich directly can be attachted as XREF.
+    """
+    if os.name != "nt":  # Check for Windows OS
+        return False
+
+    oda_path = None
+
+    programs_path = os.environ.get("ProgramFiles")
+    for programs_dir in os.listdir(programs_path):
+        # Skip files
+        if not os.path.isdir(os.path.join(programs_path, programs_dir)):
+            continue
+
+        # Check for ODA File Converter
+        if "ODA" in programs_dir:
+            oda_path = os.path.join(programs_path, programs_dir)
+            break
+
+    if not oda_path:
+        print("ODA File Converter not found")
+        return False
+
+    # Check for all available versions
+    converter_folders = glob(os.path.join(oda_path, "ODAFileConverter *"))
+    if not converter_folders:
+        print("ODA File Converter not found")
+        return False
+    # Check for the latest version
+    latest_version = max(converter_folders, key=os.path.dirname)
+
+    converter_exe = os.path.join(latest_version, "ODAFileConverter.exe")
+    input_path = os.path.abspath(os.path.dirname(dxf_path))
+    print(input_path)
+    cmd = [
+        converter_exe,
+        input_path,  # Quoted input folder
+        input_path,  # Quoted output folder
+        "ACAD2013",  # Output version
+        "DWG",  # Output File type
+        "0",  # Recurse Input Folder
+        "0",  # Audit each file
+        "*.DXF",  # Input files filter
+    ]
+    subprocess.run(cmd)
+
+    return True
+
+
+def run(ifc_file_path: str) -> None:
+    model = ifcopenshell.open(ifc_file_path)
+    dxf_file, msp = _setup_dxf()
+
     sewers = ifcopenshell.util.selector.filter_elements(
         model, "IfcFlowSegment,Haltung.ID_NAME != NULL"
     )
+    laterals = ifcopenshell.util.selector.filter_elements(
+        model, "IfcFlowTreatmentDevice,Leitung.ID_NAME != NULL"
+    )
+
     for s in sewers:
         pset = ifcopenshell.util.element.get_pset(s, "Haltung")
         if _sewer_check_slope(pset):
@@ -59,19 +125,25 @@ def run(ifc_file_path: str) -> None:
                 msp,
                 _sewer_midpoint(pset),
                 f"Gefälle: {round(pset['HYD_GRAD_PP'] * 1000, 1)} ‰",
+                color=256,
+                layer="Kanal",
             )
 
-        laterals = ifcopenshell.util.selector.filter_elements(
-            model, "IfcFlowTreatmentDevice,Leitung.ID_NAME != NULL"
-        )
-        for lateral in laterals:
-            pset = ifcopenshell.util.element.get_pset(lateral, "Leitung")
-            if _sewer_check_slope(pset):
-                _draw_check_marker(
-                    msp,
-                    _sewer_midpoint(pset),
-                    f"Gefälle: {round(pset['HYD_GRAD_PP'] * 1000, 1)} ‰",
-                )
+    for lateral in laterals:
+        pset = ifcopenshell.util.element.get_pset(lateral, "Leitung")
+        if _sewer_check_slope(pset):
+            _draw_check_marker(
+                msp,
+                _sewer_midpoint(pset),
+                f"Gefälle: {round(pset['HYD_GRAD_PP'] * 1000, 1)} ‰",
+                color=256,
+                layer="Kanal",
+            )
 
     zoom.extents(msp)
-    dxf_file.saveas(f"{ifc_file_path}.dxf".replace(".ifc", ""))
+    dxf_path = f"{ifc_file_path}.dxf".replace(".ifc", "")
+    dxf_file.saveas(dxf_path)
+
+    dwg_available = _convert_to_DWG(dxf_path)
+    if dwg_available:
+        os.remove(dxf_path)
